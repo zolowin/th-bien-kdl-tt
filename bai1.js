85099c69aa3008ae0de8125f9cc02ba267191e20
zolowin <57177546+zolowin@users.noreply.github.com>
2019-11-04 15:17:57 +0700

    thêm file

---
+++ bai1.js
@@ -0,0 +1,7 @@
+let ly = parseFloat(prompt('nhapdiemly'));
+let hoa = parseFloat(prompt('nhapdiemhoa'));
+let sinh = parseFloat(prompt('nhapdiemsinh'));
+let s = sinh + hoa + ly;
+document.write('tổng điểm 3 môn là: ' + s + '<br>')
+let a = s / 3;
+document.write('điểm trung bình 3 môn là: ' + a)
